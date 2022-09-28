Corrected Jacobian Calculation

GEModelTools/GEModelClass.py
@@ -18,8 +18,9 @@ from . import tests
 # from .simulate_hh import simulate_hh_forwards_endo, simulate_hh_forwards_exo
 from .replaced_functions import find_i_and_w_1d_1d, find_i_and_w_1d_1d_path
 from .replaced_functions import simulate_hh_forwards_endo, simulate_hh_forwards_exo
-# from .simulate_hh import simulate_hh_forwards_endo_transpose, simulate_hh_forwards_exo_transpose
 from .replaced_functions import simulate_hh_forwards_endo_transpose, simulate_hh_forwards_exo_transpose
+# TODO: delete
+from .replaced_functions import simulate_hh_forwards_endo_transpose_old, simulate_hh_forwards_exo_transpose_old
 from .simulate_hh import simulate_hh_ss, simulate_hh_path, simulate_hh_z_path
 from .broyden_solver import broyden_solver
 from .simulate import update_IRF_hh,simulate_agg,simulate_agg_hh
@@ -898,7 +899,6 @@ class GEModelClass:
         def demean(x):
             return x - x.sum()/x.size
 
-
         curly_E = {}
 
         for outputname in self.outputs_hh:
@@ -915,10 +915,32 @@ class GEModelClass:
             for outputname in self.outputs_hh:
                 temp = simulate_hh_forwards_endo_transpose(curly_E[outputname][t-1],ss.pol_indices,ss.pol_weights)
                 curly_E[outputname][t] = simulate_hh_forwards_exo_transpose(temp, ss.z_trans)
-                # simulate_hh_forwards_exo_transpose(temp,ss.z_trans,curly_E[outputname][t])
                 curly_E[outputname][t] = demean(curly_E[outputname][t])
-            
+
         if do_print: print(f'curly_E calculated in {elapsed(t0)}')
+
+
+        # TODO: delete
+        # Debugging:
+        # for the shock of z on A_hh, we know that nothing should change
+        # I.e. the expectation vector should be the same as the ss policy
+        # and (curly_E['a'][t]*curly_Dbeg1['Z'][0]).sum() = 0
+        # 1. assert that the distribution did not change along the illiquid asset grid
+        for i in range(curly_Dbeg1['Z'].shape[0]):
+            assert np.isclose(abs(curly_Dbeg1['Z'][i].sum(axis=(0,1,2))).sum(), 0, atol=1e-10), 'movement along illiquid asset grid'
+        # if the above assertion does not result in an error, the error must be a numerical error
+        # or there is an error in the expectation vectors
+        # The important part for those is that they must always have the same values for each a[i_l, :]
+        for i in range(curly_E['a'].shape[0]):
+            c_E_mean = np.empty_like(curly_E['a'][i])
+            for i_l in range(par.Nl):
+                c_E_mean[:, :, i_l, :] = curly_E['a'][i].mean(axis=-2)
+            deviation = curly_E['a'][i] - c_E_mean
+            if abs(deviation).sum() > 1e-10:
+                idx = np.where(abs(deviation) == np.max(abs(deviation)))
+                curly_E['a'][i][idx]
+        print('')
+
             
         # d. step 4: F   
         t0 = time.time()
@@ -1109,7 +1131,18 @@ class GEModelClass:
                 # i. inputs
                 x0 = x_ss.ravel().copy()
                 x0[i] += dx
-                
+
+                # store which variable is shocked
+                n_i = len(inputs)
+                if i < (x_ss.size / len(inputs)):
+                    shock_at = inputs[0] + '[' + str(i) + ']'
+                elif i < (x_ss.size / len(inputs))*2:
+                    shock_at = inputs[1] + '[' + str(i-100) + ']'
+                elif i < (x_ss.size / len(inputs))*3:
+                    shock_at = inputs[1] + '[' + str(i-200) + ']'
+                print('shock at: ' + shock_at)
+
+
                 # ii. evaluations
                 self._set_unknowns(x0,inputs)
 

GEModelTools/path.py
@@ -15,8 +15,29 @@ def lead(pathvalue,ssvalue):
 def bound(var,a,b):
     return np.fmin(np.fmax(var,a),b)
 
+
 @nb.njit
-def bisection(f,a,b,args=(),max_iter=500,tol=1e-12):
+def bisection(f, a, b, args=(), max_iter=500, tol=1e-12):
+    i = 0
+    while i < max_iter:
+
+        m = (a + b) / 2
+        fm = f(m, *args)
+
+        if abs(fm) < tol:
+            break
+        elif f(a, *args) * fm < 0:
+            b = m
+        elif f(b, *args) * fm < 0:
+            a = m
+
+        i += 1
+
+    return m
+
+# TODO: remove
+# @nb.njit
+def bisection_no_jit(f,a,b,args=(),max_iter=500,tol=1e-12):
       
     i = 0
     while i < max_iter:

GEModelTools/replaced_functions.py
@@ -379,10 +379,10 @@ def simulate_hh_forwards_endo_2d_2iw_trans(Dbeg_plus, i, w):
                     w_1_ = w[0, i_fix, i_z, i_endo1, i_endo2]
                     w_2_ = w[1, i_fix, i_z, i_endo1, i_endo2]
 
-                    D[i_fix, i_z, i_endo1, i_endo2] = ((w_1_ * w_2_) * Dbeg_plus[i_fix, i_z, i_endo1, i_endo2] + \
-                                                      + w_1_ * (1 - w_2_) * Dbeg_plus[i_fix, i_z, i_endo1, i_endo2 + 1] + \
-                                                      (1 - w_1_) * w_2_ * Dbeg_plus[i_fix, i_z, i_endo1 + 1, i_endo2] + \
-                                                      (1 - w_1_) * (1 - w_2_) * Dbeg_plus[i_fix, i_z, i_endo1 + 1, i_endo2 + 1])
+                    D[i_fix, i_z, i_endo1, i_endo2] = ((w_1_ * w_2_) * Dbeg_plus[i_fix, i_z, i_1_, i_2_] + \
+                                                      + w_1_ * (1 - w_2_) * Dbeg_plus[i_fix, i_z, i_1_, i_2_ + 1] + \
+                                                      (1 - w_1_) * w_2_ * Dbeg_plus[i_fix, i_z, i_1_ + 1, i_2_] + \
+                                                      (1 - w_1_) * (1 - w_2_) * Dbeg_plus[i_fix, i_z, i_1_ + 1, i_2_ + 1])
     return D
 
 # @nb.njit
@@ -418,4 +418,61 @@ def simulate_hh_forwards_endo_transpose(Dbeg_plus, i, w):
         raise NotImplementedError
     return D
 
+@nb.njit
+def simulate_hh_forwards_endo_transpose_old(Dbeg_plus, i, w, D):
+    """ simulate given indices and weights """
 
+    Ndim_i = i.ndim
+    Ndim_D = Dbeg_plus.ndim
+
+    if Ndim_D == 3:
+        raise NotImplementedError
+    elif Ndim_D == 4 and Ndim_i == Ndim_D:
+        raise NotImplementedError
+    elif Ndim_D == 4 and Ndim_D + 1 == Ndim_i:
+        Nfix = Dbeg_plus.shape[0]
+        Nz = Dbeg_plus.shape[1]
+        Nendo1 = Dbeg_plus.shape[2]
+        Nendo2 = Dbeg_plus.shape[3]
+        # D = np.zeros_like(Dbeg_plus)
+        for i_fix in nb.prange(Nfix):
+            for i_z in nb.prange(Nz):
+                for i_endo1 in nb.prange(Nendo1):
+                    for i_endo2 in nb.prange(Nendo2):
+                        i_1_ = i[0, i_fix, i_z, i_endo1, i_endo2]
+                        i_2_ = i[1, i_fix, i_z, i_endo1, i_endo2]
+                        w_1_ = w[0, i_fix, i_z, i_endo1, i_endo2]
+                        w_2_ = w[1, i_fix, i_z, i_endo1, i_endo2]
+
+                        D[i_fix, i_z, i_endo1, i_endo2] = ((w_1_ * w_2_) * Dbeg_plus[i_fix, i_z, i_1_, i_2_] + \
+                                                           + w_1_ * (1 - w_2_) * Dbeg_plus[
+                                                               i_fix, i_z, i_1_, i_2_ + 1] + \
+                                                           (1 - w_1_) * w_2_ * Dbeg_plus[
+                                                               i_fix, i_z, i_1_ + 1, i_2_] + \
+                                                           (1 - w_1_) * (1 - w_2_) * Dbeg_plus[
+                                                               i_fix, i_z, i_1_ + 1, i_2_ + 1])
+    else:
+        raise NotImplementedError
+
+
+@nb.njit(parallel=True)
+def simulate_hh_forwards_exo_transpose_old(Dbeg,z_trans, D):
+    """ simulate given indices and weights """
+    Nfix = Dbeg.shape[0]
+    # D = np.zeros_like(Dbeg)
+    if Dbeg.ndim < 4:  # dimensions
+        for i_fix in nb.prange(Nfix):
+            D[i_fix] = z_trans[i_fix]@Dbeg[i_fix]
+    elif Dbeg.ndim == 4:  # more dimensions than: Nfix, Nz and Na
+        Nz = Dbeg.shape[1]
+        Nl = Dbeg.shape[2]
+        Na = Dbeg.shape[3]
+        for i_fix in nb.prange(Nfix):
+            for i_l in nb.prange(Nl):
+                for i_a in nb.prange(Na):
+                    for i_z in nb.prange(Nz):
+                        D[i_fix, i_z, i_l, i_a] = 0
+                        for i_z_plus in nb.prange(Nz):
+                            D[i_fix, i_z, i_l, i_a] += z_trans[i_fix, i_z, i_z_plus] * Dbeg[i_fix, i_z_plus, i_l, i_a]
+    else:
+        raise NotImplementedError

blocks.py
@@ -2,12 +2,15 @@ import numpy as np
 import numba as nb
 
 from GEModelTools import lag, lead, bound, bisection
+# TODO: remove
+from GEModelTools.path import bisection_no_jit
 from helper_functions import integrate_marg_util
 from helper_functions import broyden_solver_cust
 from helper_functions import obtain_J
 
 
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def NKPC_eq(x, par, r, s, Pi_plus):
     gap = s - (par.e_p - 1)/par.e_p
 
@@ -30,7 +33,8 @@ def NKPC_w_eq(x, par, s_w, Pi_w_plus):
     return NKPC_w
 
 # Question: scale inv adjustment costs?
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def adj_costs(K, K_plus, phi, delta_K):
     I = K_plus - (1 - delta_K) * K
     adj_costs = phi / 2 * (I / K - delta_K) ** 2 * K
@@ -38,7 +42,8 @@ def adj_costs(K, K_plus, phi, delta_K):
     adj_costs_deriv2 = phi / K
     return adj_costs, adj_costs_deriv1, adj_costs_deriv2
 
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def inv_eq(Q, K_lag, K, K_plus, K_plus2, K_plus3, r_plus, delta_K, phi_K):
     S, S1, _ = adj_costs(K_plus, K_plus2, phi_K, delta_K)
     _, S1_plus, _ = adj_costs(K_plus2, K_plus3, phi_K, delta_K)
@@ -49,12 +54,14 @@ def inv_eq(Q, K_lag, K, K_plus, K_plus2, K_plus3, r_plus, delta_K, phi_K):
     RHS = Q + (1 / (1 + r_plus)) * (I_plus2 / I_plus) ** 2 * S1_plus
     return LHS - RHS
 
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def unpack_kwargs(d):
     """ simple unpacking funtion specific to the current residual function"""
     return d['par'], d['ss'], d['Y'], d['w'], d['r']
 
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def residual(x, kwargs_dict):
     """ residual function to optimize using the broyden solver
 
@@ -99,7 +106,8 @@ def residual(x, kwargs_dict):
     # return target1, target2
     return np.hstack((target1, target2)) # np.array((target1, target2)).reshape(-1)
 
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def flat_to_K_Q(x):
     """ Flat array into seperate arrays for K and Q"""
     nx = x.shape[0]
@@ -107,8 +115,8 @@ def flat_to_K_Q(x):
     nx_half = int(nx/2)
     return x[:nx_half], x[nx_half:]
 
-
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def block_pre(par, ini, ss, path, ncols=1):
     """ evaluate transition path - before household block """
     for ncol in nb.prange(ncols):
@@ -190,19 +198,9 @@ def block_pre(par, ini, ss, path, ncols=1):
                                     tol=1e-8, max_iter=200, backtrack_fac=0.5, max_backtrack=100,
                                     do_print=True)
 
-        # x0 = np.array([initK, initQ]).ravel()
-        # # compute jacobian to input
-        # y0 = f(x0)
-        # jac = obtain_J(f, x0, y0)
-        #
-        # # run solver
-        # x_end = broyden_solver_cust(f, x0, jac=jac,
-        #                     tol=1e-8, max_iter=200, backtrack_fac=0.5, max_backtrack=100,
-        #                     do_print=False, do_print_unknowns=False, model=None,
-        #                     fixed_jac=False)
-
         # back out K and Q
         K_opt, Q_opt = flat_to_K_Q(x_end)
+        # Question: is Q also predetermined
         K[:]= np.concatenate((np.array([ss.K, ss.K]), K_opt))  # append fixed ss values for t=0,1
         Q[:] = np.concatenate((np.array([ss.Q, ss.Q]), Q_opt))
 
@@ -247,7 +245,8 @@ def block_pre(par, ini, ss, path, ncols=1):
         for t_ in range(par.T):
             t = (par.T - 1) - t_
             Pi_plus = Pi[t + 1] if t < par.T - 1 else ss.Pi
-            Pi[t] = bisection(NKPC_eq, -0.2, 0.2, args=(par, r[t], s[t], Pi_plus))
+            # TODO: change to normal one
+            Pi[t] = bisection_no_jit(NKPC_eq, -0.2, 0.2, args=(par, r[t], s[t], Pi_plus))
 
         # c. Taylor rule
         for t in range(par.T):

helper_functions.py
@@ -41,7 +41,8 @@ def integrate_marg_util(c, D, z_grid, sigma):
 # "TypingError: Failed in nopython mode pipeline (step: convert make_function into JIT functions)
 # Cannot capture the non-constant value associated with variable 'Y' in a function that will escape."
 # "AssertionError: Failed in nopython mode pipeline (step: inline calls to locally defined closures)"
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def broyden_solver_cust(f, x0, kwargs_dict=None, jac=None,
                         tol=1e-8, max_iter=100, backtrack_fac=0.5, max_backtrack=30,
                         do_print=False):
@@ -68,7 +69,7 @@ def broyden_solver_cust(f, x0, kwargs_dict=None, jac=None,
 
         if abs_diff < tol: return x
 
-        # # init jac of neccessary
+        # # init jac not neccessary
         # if not isinstance(jac, np.ndarray):
         #     # initialize J with Newton!
         #     if jac == None and kwargs_dict == None:
@@ -109,7 +110,8 @@ def broyden_solver_cust(f, x0, kwargs_dict=None, jac=None,
 
         raise ValueError('GEModelTools: No convergence of broyden solver in solving for investment')
 
-@nb.njit
+# TODO: remove comment
+# @nb.njit
 def obtain_J(f, x, y, kwargs_dict=None, h=1E-5):
     """Finds Jacobian f'(x) around y=f(x)"""
     nx = x.shape[0]